f31db35108c207b57b991be652ea46f07bf6cfef
simetrio <simetrio@yandex.com>
2024-06-28 01:01:14 +0500

    Загружаю все данные в одном месте

--- src/uniswap-app/src/index.js
+++ src/uniswap-app/src/index.js
@@ -14,9 +14,11 @@ window.uniswapAppMain = async function () {
         console.log(config.wallet.address)
         setStatus("Загрузка...")
 
-        await Promise.all([loadBalance(config), loadPosition(config)])
+        const data = await loadData(config)
+        showBalance(config, data)
+        showPosition(config, data)
 
-        setStatus(new Date().toLocaleString())
+        setStatus(new Date().toLocaleTimeString())
     } catch {
         setStatus("Ошибка!!!")
     }
@@ -44,44 +46,52 @@ function getConfig() {
     }
 }
 
-async function loadBalance(config) {
-    const [coin0Balance, coin1Balance, ethBalance] = await Promise.all([
+async function loadData(config) {
+    const [coin0Balance, coin1Balance, ethBalance, position, priceCurrent] = await Promise.all([
         uniswap.getBalance(config, config.coin0),
         uniswap.getBalance(config, config.coin1),
         uniswap.getEthBalance(config),
-    ])
-
-    console.log("Balance", config.coin0.name, coin0Balance)
-    console.log("Balance", config.coin1.name, coin1Balance)
-    console.log("Balance", "ETH", ethBalance)
-
-    document.getElementById("coin0-name").innerText = config.coin0.name
-    document.getElementById("coin0").innerText = coin0Balance
-    document.getElementById("coin1-name").innerText = config.coin1.name
-    document.getElementById("coin1").innerText = coin1Balance
-    document.getElementById("eth").innerText = ethBalance
-}
-
-async function loadPosition(config) {
-    const [position, priceCurrent] = await Promise.all([
         uniswap.getLastPosition(config),
         uniswap.getPrice(config),
     ])
-    console.log("Position", position, position.liquidity.toString())
 
-    document.getElementById("position-number").innerText = position.id
+    return {
+        coin0Balance,
+        coin1Balance,
+        ethBalance,
+        position,
+        priceCurrent,
+    }
+}
 
-    if (position.liquidity.toString() === "0") {
+function showBalance(config, data) {
+    console.log("Balance", config.coin0.name, data.coin0Balance)
+    console.log("Balance", config.coin1.name, data.coin1Balance)
+    console.log("Balance", "ETH", data.ethBalance)
+
+    document.getElementById("coin0-name").innerText = config.coin0.name
+    document.getElementById("coin0").innerText = data.coin0Balance
+    document.getElementById("coin1-name").innerText = config.coin1.name
+    document.getElementById("coin1").innerText = data.coin1Balance
+    document.getElementById("eth").innerText = data.ethBalance
+}
+
+function showPosition(config, data) {
+    console.log("Position", data.position, data.position.liquidity.toString())
+
+    document.getElementById("position-number").innerText = data.position.id
+
+    if (data.position.liquidity.toString() === "0") {
         console.log("Position closed")
         document.getElementById("position-status").innerText = "Закрыта"
         return
     }
 
-    const priceUpper = uniswap.getPriceFromTick(config, position.tickUpper)
-    const priceLower = uniswap.getPriceFromTick(config, position.tickLower)
+    const priceUpper = uniswap.getPriceFromTick(config, data.position.tickUpper)
+    const priceLower = uniswap.getPriceFromTick(config, data.position.tickLower)
 
-    const isInRange = priceCurrent > priceLower && priceCurrent < priceUpper
-    const isUpper = !isInRange && priceCurrent > priceUpper
+    const isInRange = data.priceCurrent > priceLower && data.priceCurrent < priceUpper
+    const isUpper = !isInRange && data.priceCurrent > priceUpper
 
     // View
 
@@ -89,11 +99,11 @@ async function loadPosition(config) {
 
     let priceHtml = ""
     if (isInRange) {
-        priceHtml = `${html.textGray(html.price(priceLower))} &mdash; ${html.textGreen(html.price(priceCurrent))} &mdash; ${html.textGray(html.price(priceUpper))}`
-    } else if(isUpper) {
-        priceHtml = `${html.textGray(html.price(priceLower))} &mdash; ${html.textGray(html.price(priceUpper))} &mdash; ${html.textRed(html.price(priceCurrent))}`
+        priceHtml = `${html.textGray(html.price(priceLower))} &mdash; ${html.textGreen(html.price(data.priceCurrent))} &mdash; ${html.textGray(html.price(priceUpper))}`
+    } else if (isUpper) {
+        priceHtml = `${html.textGray(html.price(priceLower))} &mdash; ${html.textGray(html.price(priceUpper))} &mdash; ${html.textRed(html.price(data.priceCurrent))}`
     } else {
-        priceHtml = `${html.textRed(html.price(priceCurrent))} &mdash; ${html.textGray(html.price(priceLower))} &mdash; ${html.textGray(html.price(priceUpper))}`
+        priceHtml = `${html.textRed(html.price(data.priceCurrent))} &mdash; ${html.textGray(html.price(priceLower))} &mdash; ${html.textGray(html.price(priceUpper))}`
     }
 
     document.getElementById("position-price").innerHTML = priceHtml
