f6e65c93e7ffed62e43e3506b0cbf794076a435b
simetrio <simetrio@yandex.com>
2024-06-28 09:15:18 +0500

    Уношу исходники в друго

--- src/uniswap-app/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "uniswap.js",
-    path: path.resolve(__dirname, "..", "..", "docs", "uniswap-app")
-  }
-};
